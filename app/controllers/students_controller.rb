3793ee5031ec72eab578771f0af7f6cd1c1f1e58
torianne02 <victoria.fluharty@gmail.com>
2020-01-22 13:23:39 -0800

    adds waitlist logic to course model adds conditional logic for enrolling

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,8 +10,16 @@ class StudentsController < ApplicationController
   end
 
   post '/students/:id/enroll'
-    student = Student.all.find { |s| s.id == params[:id] }
-    course = Course.all.find { |c| c.key == params['course_key']}
-    
-    student.courses << 
+    @student = Student.all.find { |s| s.id == params[:id] }
+    @course = Course.all.find { |c| c.key == params['course_key']}
+
+    if @student.hit_limit? == false && @course.hit_limit? == false
+      @student.courses << [@course, 'Enrolled']
+      @course.students << @student
+    elsif @student.hit_limit == false && @course.hit_limit? == true
+      @student.courses << [@course, 'Waitlisted']
+      @
+    else 
+      flash[:message] = "Sorry! It looks like this student has hit their course limit"
+    end 
 end 
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course
   attr_reader :key, :limit
+  attr_accessor :students
 
   @@all = []
 
@@ -7,6 +8,7 @@ class Course
     @key = key
     @limit = limit
     @students = []
+    @waitlist = {}
     @@all << self
   end 
 
@@ -16,5 +18,11 @@ class Course
 
   def hit_limit?
     @students.length < @limit ? false : true
+  end
+
+  def add_to_waitlist(student)
+    # adds student name and waitlist position
+    # not full proof adding length as key // if someone where to be removed from waitlist I'd need to update all keys by -= 1
+    @waitlist[student] = @waitlist.length
   end
 end 
