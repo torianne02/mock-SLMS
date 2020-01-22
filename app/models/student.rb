b7decf86858e1917b35d7b04c870f570e85f8579
torianne02 <victoria.fluharty@gmail.com>
2020-01-22 12:27:59 -0800

    adds @@all class variable to keep track of all students during a session

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,10 +2,17 @@ class Student
   attr_reader :name, :limit
   attr_accessor :courses
 
+  @@all = []
+
   def initialize(name, limit)
     @name = name
     @limit = limit
     @courses = []
+    @@all << self
+  end 
+
+  def self.all
+    @@all
   end 
 end 
 
